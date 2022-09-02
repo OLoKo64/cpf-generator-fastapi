f26a609e95e5fe340878eb5c7bac31645bfcf9df
OLoKo64 <naldoepingo1@gmail.com>
2022-09-02 10:31:22 -0300

    feat: add multiple validator route

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+from typing import List
 from fastapi import FastAPI
 from cpf_gen.utils import generate_cpf, validate_cpf
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+
+class CpfList(BaseModel):
+    cpf_list: List
 
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
     allow_headers=["*"],
 )
 
@@ -29,17 +34,31 @@ async def cpf_generator(qtd: int = 1):
 @app.get("/cpf/{state_code}")
 async def cpf_generator_state(state_code: int = None, qtd: int = 1):
     if state_code < 0 or state_code > 9:
-            return { "error": { "message": "Invalid CPF state code, state code must be a number between 0 and 9", "code": 408 } }
+        return {"error": {"message": "Invalid CPF state code, state code must be a number between 0 and 9", "code": 408}}
     if qtd > 100:
-        return { "error": { "message": "Invalid CPF quantity, quantity must be a number between 1 and 100", "code": 410 } }
+        return {"error": {"message": "Invalid CPF quantity, quantity must be a number between 1 and 100", "code": 410}}
     return [generate_cpf(state=state_code) for _ in range(qtd)]
 
 
-# Verify CPF
+# Validate CPF
 @app.get("/validate_cpf/{cpf}")
 async def cpf_validator(cpf: str):
-    result =  validate_cpf(cpf)
+    result = validate_cpf(cpf)
     if result:
         return {"is-valid": True, "cpf": result, "message": "Valid CPF"}
     else:
-        return { "is_valid": False, "cpf": None, "error": { "message": "Invalid CPF", "code": 406 } }
+        return {"is_valid": False, "cpf": None, "error": {"message": "Invalid CPF", "code": 406, "cpf": cpf}}
+
+
+# Validate multiple CPFs
+@app.post("/validate_cpf")
+async def cpf_validator_multiple(data: CpfList):
+    print(len(data.cpf_list))
+    if len(data.cpf_list) > 100:
+        return {"error": {"message": "Invalid CPF quantity, quantity must be a number between 1 and 100", "code": 410}}
+    return [
+        {"is-valid": True, "cpf": validate_cpf(cpf), "message": "Valid CPF"}
+        if validate_cpf(cpf)
+        else {"is_valid": False, "cpf": None, "error": {"message": "Invalid CPF", "code": 406, "cpf": cpf}}
+        for cpf in data.cpf_list
+    ]
